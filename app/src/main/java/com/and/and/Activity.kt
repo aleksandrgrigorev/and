ecb6d664c1d1900d14c958eee1bfabe5e4fd65cf
aleksandrgrigorev <xundr1992@gmail.com>
2023-02-23 17:59:06 +0400

    FAT-52165: DrawerLayout

--- app/src/main/java/com/and/and/Activity.kt
+++ app/src/main/java/com/and/and/Activity.kt
@@ -1,14 +1,44 @@
 package com.and.and
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class Activity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction().replace(R.id.frame_layout, ThirdFragment())
-            .commit()
+//        supportFragmentManager.beginTransaction().replace(R.id.frame_layout, ThirdFragment())
+//            .commit()
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_lesson_1 -> TODO()
+                R.id.menu_lesson_2 -> TODO()
+                R.id.menu_lesson_3 -> TODO()
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
